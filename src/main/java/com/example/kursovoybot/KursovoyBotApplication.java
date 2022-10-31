69696f8a98b0cdd453346a0bccc455dc7ed2962f
6564747347456845 <webkonditer@yandex.ru>
2022-10-31 18:20:56 +0300

    ready bot

---
+++ src/main/java/com/example/kursovoybot/KursovoyBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.kursovoybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovoyBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursovoyBotApplication.class, args);
+    }
+
+}
