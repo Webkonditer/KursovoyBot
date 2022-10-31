586322f6749d255f0af087d25c37e9308b76e482
6564747347456845 <webkonditer@yandex.ru>
2022-10-31 18:30:36 +0300

    init

--- src/main/java/com/example/kursovoybot/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.kursovoybot.repository;
-
-import com.example.kursovoybot.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
